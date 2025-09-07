226de9a06e0200c9d4614b0ed2f24f73d20aa617
TheYafen <theyafen@gmail.com>
2025-09-07 15:09:45 +0300

    First commit: bot, Dockerfile, requirements

---
+++ bot.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, Router, types, F
+from aiogram.filters import Command
+import asyncio
+import os
+
+TOKEN = '1635830103:AAEeYsKab_zFVxoSMfPWjMG9acTUg58TR-M'
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+router = Router()
+dp.include_router(router)
+
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("THis message is from Bot")
+
+@dp.message(Command("do"))
+async def cmd_start(message: types.Message):
+    await message.answer("THis message is from Bot")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
